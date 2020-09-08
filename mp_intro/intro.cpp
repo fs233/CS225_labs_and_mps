0b2bf6fb9e8bbbcad9ce9e7202a57e81e8b5bb78
Haomiao Chen <haomiao4@linux-09.ews.illinois.edu>
2020-09-08 13:19:33 -0500

    mp_intro

--- mp_intro/intro.cpp
+++ mp_intro/intro.cpp
@@ -9,25 +9,14 @@ void rotate(std::string inputFile, std::string outputFile) {
   png.readFromFile(inputFile);
   unsigned a = png.width();
   unsigned b = png.height();
-  if(!(a%2==0)){
-    for (unsigned y = 0; y < b/2 ; y++) {
-      cs225::HSLAPixel & pixel1 = png.getPixel(a/2, y);
-      cs225::HSLAPixel & pixel2 = png.getPixel(a/2, b-y-1);
-      cs225::HSLAPixel pixelt = pixel1;
-      pixel1 = pixel2;
-      pixel2 = pixelt;
-    }
-  }
-  for (unsigned x = 0; x < a/2; x++) {
+  cs225::PNG newpng(a,b);
+  for (unsigned x = 0; x < a; x++) {
     for (unsigned y = 0; y < b; y++) {
-      cs225::HSLAPixel & pixel1 = png.getPixel(x, y);
-      cs225::HSLAPixel & pixel2 = png.getPixel(a-x-1, b-y-1);
-      cs225::HSLAPixel pixelt = pixel1;
-      pixel1 = pixel2;
-      pixel2 = pixelt;
+      cs225::HSLAPixel pixel = png.getPixel(x, y);
+      newpng.getPixel(a-x-1, b-y-1)=pixel;
     }
   }
-  png.writeToFile(outputFile);
+  newpng.writeToFile(outputFile);
 }
 
 cs225::PNG myArt(unsigned int width, unsigned int height) {
