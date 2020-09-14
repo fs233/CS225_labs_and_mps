f3c6afc717b6f964c736df481d5dbce99cb1cce0
Haomiao Chen <haomiao4@linux-34.ews.illinois.edu>
2020-09-13 20:58:19 -0500

    mp_stickers

--- lab_memory/letter.h
+++ lab_memory/letter.h
@@ -38,3 +38,18 @@ class Letter
      */
     bool operator<(const Letter& other) const;
 };
+
+
+class StickerSheet : public cs225::Image{
+public:
+  StickerSheet (const Image &picture, unsigned max);
+  ~StickerSheet ();
+  StickerSheet (const StickerSheet &other);
+  //const StickerSheet & operator= (const StickerSheet &other);
+  void changeMaxStickers (unsigned max);
+  int addSticker (Image &sticker, unsigned x, unsigned y);
+  bool translate (unsigned index, unsigned x, unsigned y);
+  void removeSticker (unsigned index);
+  Image* getSticker(unsigned index);
+  Image render()const;
+}
