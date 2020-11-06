668c0432b3ad103ac9799c58ef5a2cfbfc9c7775
Haomiao Chen <haomiao4@linux-03.ews.illinois.edu>
2020-11-05 19:44:52 -0600

    mp_mazes

--- mp_mazes/dsets.cpp
+++ mp_mazes/dsets.cpp
@@ -1,1 +1,47 @@
 /* Your code here! */
+#include "dsets.h"
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void DisjointSets::addelements(int num){
+    for(int i = 0; i<num; i++){
+        up_tree.push_back(-1);
+    }
+}	
+
+int DisjointSets::find(int elem){//finding the index of the root of the tree
+    if(up_tree[elem]<0){
+        return elem;
+    }else{
+        int root = find(up_tree[elem]);
+        up_tree[elem]=root;
+        return root;
+    }
+}	
+
+
+void DisjointSets::setunion	(int a, int b){
+    if(find(a)==find(b)){
+        return;
+    }
+    int roota = up_tree[find(a)];//size of set a
+    int rootb = up_tree[find(b)];//size of set b
+    int newSize = roota+rootb;
+    if(roota > rootb){//a is samller than b
+        up_tree[find(a)] = find(b);
+        up_tree[find(b)] = newSize;
+    }else{
+        up_tree[find(b)] = find(a);
+        up_tree[find(a)] = newSize;
+    }
+}		
+
+
+int DisjointSets::size	(int elem){
+    int index = find(elem);
+    return -up_tree[index];
+}	
+
+
+
